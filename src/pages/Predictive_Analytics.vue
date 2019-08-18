62a5181c2a62ac9ed22fe13ed3de7509f76056e1
PSeverina <severich@yandex.ru>
2019-08-18 22:32:22 +0300

    updates

--- src/pages/Predictive_Analytics.vue
+++ src/pages/Predictive_Analytics.vue
@@ -1,19 +1,19 @@
 <template>
  <div class="stars font">
     <div class="twinkling">
-      <h1 class="header">Predictive Analytics</h1>
-      <hr class="hr pred-analytics__hr">
-      <div class="pred-analytics__header-2"> 
+      <h1 class="predAnalytics__header header">Predictive Analytics</h1>
+      <hr class="hr predAnalytics__hr">
+      <div class="predAnalytics__header-2"> 
         What is the difference between AI,
         Machine Learning, & Deep Learning ?
       </div>
-      <img src='../img/pred_analytics/AI_subsets_black.png' class="pred-analytics__schema-1" v-viewer />
-      <div class="pred-analytics__text"> Artificial Intelligence, Machine Learning,& Deep Learning are nested subsets <br>
+      <img src='../img/pred_analytics/AI_subsets_black.png' class="predAnalytics__schema-1" v-viewer />
+      <div class="predAnalytics__text"> Artificial Intelligence, Machine Learning,& Deep Learning are nested subsets <br>
           of each other, as deplicated in the diagram above.
       </div>
-      <hr class="hr pred-analytics__hr-2">
-      <img src='../img/pred_analytics/machine_learning_black.png' class="pred-analytics__schema-2" v-viewer />
-      <div class="pred-analytics__header-2">
+      <hr class="hr predAnalytics__hr-2">
+      <img src='../img/pred_analytics/machine_learning_black.png' class="predAnalytics__schema-2" v-viewer />
+      <div class="predAnalytics__header-2">
           Machine Learning is the most promising <br>
           tool in the AI category for businesses.
       </div>
--- src/pages/Prescriptive_Analytics.vue
+++ src/pages/Prescriptive_Analytics.vue
@@ -1,19 +1,19 @@
 <template>
- <div class="stars font">
+ <div class="container-fluid stars font">
     <div class="twinkling">
-      <h1 class="header">Prescriptive Analytics</h1>
-      <hr class="hr pres-analytics__hr">
-      <div class="pres-analytics__header-2">
+      <h1 class="presAnalytics__header header">Prescriptive Analytics</h1>
+      <hr class="hr presAnalytics__hr">
+      <div class="presAnalytics__header-2">
         Given what's happened / what we think will happen,<br>
          what is the best action to take?
       </div>
-      <img alt="schema_1" class="pres-analytics__schema-1" src="../img/pres_analytics/prescriptive_modeling_1.png" v-viewer/>
-      <div class="pres-analytics__header-2">
+      <img alt="schema_1" class="presAnalytics__schema-1" src="../img/pres_analytics/prescriptive_modeling_1.png" v-viewer/>
+      <div class="presAnalytics__header-2">
         Optimization Mathematical Modeling<br>
         solves complex problems the human mind cannot
       </div>
-      <img alt="schema_2" class="pres-analytics__schema-2" src="../img/pres_analytics/or_1.png" v-viewer/>
-      <div class="pres-analytics__text">
+      <img alt="schema_2" class="presAnalytics__schema-2" src="../img/pres_analytics/or_1.png" v-viewer/>
+      <div class="presAnalytics__text">
         A&nbsp;custom mathematical algorithm is&nbsp;built to&nbsp;represent the problem at&nbsp;hand, 
         considering all unique requirements and objectives. The algorithm generates millions of&nbsp;all 
         possible solutions, narrowing down the possibilities that satisfy constraints, ultimately 
