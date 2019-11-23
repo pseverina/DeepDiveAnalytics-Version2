5f35ddad9daba74c4656cf3016d06f5f50d6ed77
PSeverina <severich@yandex.ru>
2019-11-24 00:30:24 +0300

    added BEM changes to a home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,7 @@
      <div class="twinkling">
        <!-- First section -->
        <div class="transforCodeMD">
-         <h1 class="transforCodeMD__head">CodeMD is Transformational</h1>
+         <h1 class="transforCodeMD__title">CodeMD is Transformational</h1>
          <p class="transforCodeMD__text">
             Advanced analytics for health care providers.<br>
             Automated provider scheduling solution based on&nbsp;unique rules and requirements.<br>
@@ -17,7 +17,7 @@
        </div>
         <!--  Second Section -->
         <div class="supplyChain">
-          <h2 class="header-small supplyChain__head">Driving Performance for Supply Chain</h2>
+          <h2 class="supplyChain__title">Driving Performance for Supply Chain</h2>
           <p class="supplyChain__text">
             Make the system smarter with Deep Dive Analytics.<br>
             End-to-End supply chain solutions.<br>
@@ -31,7 +31,7 @@
         </div>
         <!-- Third Section -->
         <div class="industryAgnostic">
-          <h2 class="industryAgnostic__head">Industry Agnostic</h2>
+          <h2 class="industryAgnostic__title">Industry Agnostic</h2>
           <p class="industryAgnostic__text">
             Any industry. Any business.<br>
             We&nbsp;drive business advantage by delivering<br>
@@ -46,7 +46,7 @@
         </div>
         <!-- Fourth Section -->
         <div class="agileManagement">
-          <h2 class="agileManagement__head">Agile Project Management</h2>
+          <h2 class="agileManagement__title">Agile Project Management</h2>
           <p class="agileManagement__text">
             Innovate and deliver faster with Deep Dive Analytics.<br>
             Experience a&nbsp;professional and effecient working engagement.<br>
@@ -55,10 +55,11 @@
             <router-link to="/contact" class="homelink">Contact us ></router-link>
           </div>
           <b-row class="agileManagement__row">
-            <b-col class="agileManagement__col" sm>
-              <div class="agileManagement__img-left">
-                <span class="agileManagement__img-left-head blue">Define a Strategy</span>
-                <p class="agileManagement__img-left-text">
+            <b-col class="agileManagement__col agileManagement__col-left" sm>
+              <div class="agileManagement__card">
+                <img src="../assets/styles/img/home_page_img/strategy.png" class="agileManagement__left-img"/><br>
+                <span class="agileManagement__card-title">Define a Strategy</span>
+                <p class="agileManagement__card-text">
                   Assess the opportunity, 
                   thinking strategically and
                   holistically when defining the plan
@@ -66,22 +67,20 @@
                 </p>
               </div>
             </b-col>
-            <b-col class="agileManagement__col" sm>
-              <div class="agileManagement__img-middle">
-                <img class="agileManagement__img-middle-img" src="../assets/styles/img/home_page_img/iterative_development.png"/><br><br>
-                <span class="agileManagement__img-middle-head blue">Iterative Development</span>
-                <p class="agileManagement__img-middle-text">
+            <b-col class="agileManagement__col agileManagement__col-middle" sm>
+              <div class="agileManagement__card">
+                <span class="agileManagement__card-title">Iterative Development</span>
+                <p class="agileManagement__card-text">
                   Working in&nbsp;agile sprints to&nbsp;define, 
                   build, and deploy your solution so&nbsp;your 
                   business can reap the benefits quickly.
                 </p>
               </div>
             </b-col>
-            <b-col class="agileManagement__col" sm>
-              <div class="agileManagement__img-right">
-                <img class="agileManagement__img-right-img" src="../assets/styles/img/home_page_img/business_impact.png"/><br><br>
-                <span class="agileManagement__img-right-head blue">Deliver Business Impact</span>
-                <p class="agileManagement__img-right-text">
+            <b-col class="agileManagement__col agileManagement__col-right" sm>
+              <div class="agileManagement__card">
+                <span class="agileManagement__card-title">Deliver Business Impact</span>
+                <p class="agileManagement__card-text">
                   Driving performance and
                   efficiency across your
                   organization through advanced
@@ -93,31 +92,27 @@
         </div>
         <!--  Fifth Section -->
         <div class="deepDive">
-          <div class="deepDive__head">
-            <div class="deepDive__head-green">Deep Dive </div>
-            <div class="deepDive__head-white">into your data to make </div>
-            <div class="deepDive__head-green">Smarter Decisions</div>.
-          </div> 
-          <b-row>
-            <b-col sm="6">
-              <p class="deepDive__text">
-                We&nbsp;combine business knowledge with advanced 
-                analytics, building custom analytical solutions based 
-                on&nbsp;your unique business needs. We&nbsp;have a&nbsp;proven 
-                track record of&nbsp;delivering business value.
-              </p>
-              <p class="deepDive__text">
-                Let&rsquo;s Deep Dive into the black hole of&nbsp;your data.<br>
-              </p>
-              <p class="deepDive__btn">
-                <router-link to="/contact" class="homelink">Contact us ></router-link>
-              </p>
-            </b-col>
-            <b-col sm="6">
-              <img class="deepDive__blackhole" alt="blackhole" src="../assets/styles/img/bg_img/black_hole.png"/> 
-              <img class="deepDive__spaceman" alt="spaceman" src="../assets/styles/img/home_page_img/blackhole_spaceman.png"/>
-            </b-col>
-          </b-row>
+          <div class="deepDive__content">
+            <div class="deepDive__title">
+              <span class="deepDive__title-green">Deep Dive </span>
+              <span class="deepDive__title-white"> into your data to make </span>
+              <span class="deepDive__title-green">Smarter Decisions</span>.
+            </div> 
+            <p class="deepDive__text">
+              We&nbsp;combine business knowledge with advanced 
+              analytics, building custom analytical solutions based 
+              on&nbsp;your unique business needs. We&nbsp;have a&nbsp;proven 
+              track record of&nbsp;delivering business value.
+            </p>
+            <p class="deepDive__text">
+              Let&rsquo;s Deep Dive into the black hole of&nbsp;your data.<br>
+            </p>
+            <p class="deepDive__btn">
+              <router-link to="/contact" class="homelink">Contact us ></router-link>
+            </p>
+            <!-- <img class="deepDive__blackhole" alt="blackhole" src="../assets/styles/img/bg_img/black_hole.png"/> 
+              <img class="deepDive__spaceman" alt="spaceman" src="../assets/styles/img/home_page_img/blackhole_spaceman.png"/> -->
+          </div>
         </div>
       </div>
     </div>
@@ -127,49 +122,8 @@
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      visible: false,
-      quote1: false,
-      quote2: false,
-      count: 0,
-      words: ['BETTER', 'SMARTER', 'QUICKER', 'TRUSTED'],
-      quotes:[
-        { id: '“More of these data-driven projects should take place throughout J&J” - Everyone , President at Company Name 1 !' },
-        { id: '“More of these data-driven projects should take place throughout J&J” - Everyone , President at Company Name 2 !' }
-      ]
-    }
-  },
-  mounted(){
-   window.setInterval(()=>{
-      this.changeWords()
-      this.count += 1
-      if(this.count % 2 == 0){
-        this.quote1 = true
-        this.quote2 = false
-      } else {
-        this.quote1 = false
-        this.quote2 = true
-      }
-    }, 2000)
-  },
-  methods: {
-    changeWords(){
-      const first = this.words.shift()
-      this.words = this.words.concat(first)
-    }
-  }
 }
 </script>
-
-<style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
-  opacity: 0;
-}
-</style>
 
 
 
